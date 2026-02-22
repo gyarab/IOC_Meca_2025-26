6428f6906562008e3c24d603d17f101876c576e3
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-02-22 19:57:01 +0100

    Refactor Alliance enum to improve structure

--- src/Chess/Alliance.java
+++ src/Chess/Alliance.java
@@ -9,81 +9,84 @@ package Chess;
  * @author mecova
  */
 public enum Alliance {
-WHITE {
-    
-    @Override
-    public int getDirection(){
-        return -1;
-    }
-    
-    @Override
-    public int getOppositeDirection(){
-        return 1;
-    }
-    
-    @Override
-    public boolean isWhite(){
-        return true;
-    }
-    
-    @Override
-    public boolean isBlack(){
-        return false;
-    }
-    
-    @Override 
-    public boolean isPawnPromotionSquare(int position){
-        return false; // default value
-    }
-    
-    @Override
-    public Player choosePlayer(final Whiteplayer whitePlayer,
-            final BlackPlayer blackPlayer){
-        return whitePlayer;
-    }
-},
-BLACK{
-    @Override
-    public int getDirection(){
-        return 1;
-    }
-    
-    @Override
-    public int getOppositeDirection(){
-        return -1;
-    }
-    
-    @Override 
-    public boolean isWhite(){
-        return false;
-    }
-    
-    @Override
-    public boolean isBlack(){
-        return true;
-    }
-    
-    @Override 
-    public boolean isPawnPromotionSquare(int position){
-        return false; // default value
-    }   
-    
-    @Override 
-    public Player choosePlayer(final Whiteplayer whitePlayer,
-            final BlackPlayer blackPlayer){
-        return blackPlayer;
-    
+    WHITE {
+
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+
+        @Override
+        public int getOppositeDirection() {
+            return 1;
+        }
+
+        @Override
+        public boolean isWhite() {
+            return true;
+        }
+
+        @Override
+        public boolean isBlack() {
+            return false;
+        }
+
+        @Override
+        public boolean isPawnPromotionSquare(int position) {
+            return false; // default value
+        }
+
+        @Override
+        public Player choosePlayer(final WhitePlayer whitePlayer,
+                final BlackPlayer blackPlayer) {
+            return whitePlayer;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+
+        @Override
+        public int getOppositeDirection() {
+            return -1;
+        }
+
+        @Override
+        public boolean isWhite() {
+            return false;
+        }
+
+        @Override
+        public boolean isBlack() {
+            return true;
+        }
+
+        @Override
+        public boolean isPawnPromotionSquare(int position) {
+            return false; // default value
+        }
+
+        @Override
+        public Player choosePlayer(final WhitePlayer whitePlayer,
+                final BlackPlayer blackPlayer) {
+            return blackPlayer;
+
+        }
+    };
+
+    public abstract int getDirection();
+
+    public abstract int getOppositeDirection();
+
+    public abstract boolean isWhite();
+
+    public abstract boolean isBlack();
+
+    public abstract boolean isPawnPromotionSquare(int position);
+
+    public abstract Player choosePlayer(final WhitePlayer whitePlayer,
+            final BlackPlayer blackPlayer);
+
 }
-};
-
-public abstract int getDirection();
-public abstract int getOppositeDirection();
-public abstract boolean isWhite();
-public abstract boolean isBlack();
-public abstract boolean isPawnPromotionSquare(int position);
-public abstract Player choosePlayer(final Whiteplayer whitePlayer,
-                                    final BlackPlayer blackPlayer);
-
-}
-
-
