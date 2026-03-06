07f1cd2f77af38875d8bc2485fdf1b26a2df0111
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-03-06 16:17:56 +0100

    Update Chesswindowpanel.java

--- src/Chess/Chesswindowpanel.java
+++ src/Chess/Chesswindowpanel.java
@@ -1,7 +1,7 @@
-  /*
-     * To change this license header, choose License Headers in Project Properties.
-     * To change this template file, choose Tools | Templates
-     * and open the template in the editor.
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Chess;
 
@@ -1375,3 +1375,4 @@ private void button5() {
     }
 
 }
+
