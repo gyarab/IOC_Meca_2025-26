038a6260ede244625f670a8d8f25ea06ba5a680d
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-03-23 23:38:26 +0100

    Update Types.java

--- src/Chess/Pieces/piece/Types.java
+++ src/Chess/Pieces/piece/Types.java
@@ -10,28 +10,34 @@ package Chess.Pieces.piece;
  * @author Admin
  */
 public enum Types {
-      
-   PAWN(100),
-   KNIGHT(300),
-   BISHOP(330),
-   ROOK(500),
-   QUEEN(900),
-   KING(10000);
+   
+    PAWN(100, "P"),
+    KNIGHT(300, "N"),
+    BISHOP(330, "B"),
+    ROOK(500, "R"),
+    QUEEN(900, "Q"),
+    KING(1000, "K");
    
    public Types types;
    private final int value;
-   
+   private final String pieceName;
    
    public int getPieceValue() {
        return this.value;
+   }
+
+   @Override
+   public String toString() {
+       return this.pieceName;
    }
    
    public Types getPieceType() {
        return this.types;
    }
    
-   private Types(final int value){
+   private Types(final int value, final String pieceName){
        this.value = value;
+       this.pieceName = pieceName;
    }   
 }
 
